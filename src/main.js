041b5ba629c4f98727befe8c56e142c7ee0124da
carolchyang <chingheui.yang@gmail.com>
2021-04-11 10:57:03 +0800

    載入套件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import jquery from 'jquery';
+import 'bootstrap';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+import VueSweetalert2 from 'vue-sweetalert2';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+// jquery 設定
+window.$ = jquery;
+
+// vue-axios 設定
+Vue.use(VueAxios, axios);
+
+// vue-loading-overlay 設定
+Vue.component('Loading', Loading);
+
+// vue-sweetalert2 設定
+Vue.use(VueSweetalert2);
 
 Vue.config.productionTip = false;
 
