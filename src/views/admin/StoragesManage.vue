e3daa4460c0bbb86ab49f716606ad51f67d2cccf
carolchyang <chingheui.yang@gmail.com>
2021-04-13 11:30:15 +0800

    新增 StoragesManage 頁面

--- src/views/admin/StoragesManage.vue
+++ src/views/admin/StoragesManage.vue
@@ -1,13 +1,93 @@
 <template>
-  <div>storages</div>
+    <div class="container-fluid">
+
+    <div class="row">
+      <div class="col-12 col-lg-6 col-xl-4" v-for="item in storages" :key="item.id">
+        <div class="card mb-4">
+          <div class="card-img" :style="{backgroundImage: 'url(' + item.path + ')'}"></div>
+          <div class="card-body text-center">
+            <div class="form-group">
+              <label for="imageUrl">圖片網址</label>
+              <input type="imageUrl" class="form-control" id="imageUrl"
+               :value="item.path" disabled>
+            </div>
+            <button type="button" class="btn btn-outline-danger"
+             @click.prevent="openDelModal(item)">
+              刪除
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- delModal -->
+    <div class="modal fade" id="delModal" role="dialog" aria-labelledby="delModalTitle"
+     aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header bg-danger text-light">
+            <h5 class="modal-title" id="delModalTitle">刪除圖片</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            確定要刪除該筆資料 (刪除後無法復原)
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-dark" data-dismiss="modal">
+              取消
+            </button>
+            <button type="button" class="btn btn-outline-danger"
+             @click.prevent="delStorage()">
+              刪除
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <Pagination :pagination="pagination" @get-data="getStorages"/>
+  </div>
 </template>
 
 <script>
+/* global $ */
+import Pagination from '@/components/Pagination.vue';
+
 export default {
   name: 'StoragesManage',
+  data() {
+    return {
+      pagination: {},
+      storages: {},
+      tempStorage: {},
+    };
+  },
+  methods: {
+    getStorages() {
+    },
+    delStorage() {
+    },
+    openDelModal(item) {
+      $('#delModal').modal('show');
+      this.tempStorage = { ...item };
+    },
+  },
+  components: {
+    Pagination,
+  },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@import '@/assets/scss/all';
 
+.card-img {
+  max-width: 100%;
+  height: 12rem;
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+}
 </style>
